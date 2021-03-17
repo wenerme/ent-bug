218ec078502ac8cd89e3701e2ae49fcda1fafa22
wener <wenermail@gmail.com>
2021-03-17 15:25:15 +0800

    first commit

---
+++ ent/schema/mixin.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"entgo.io/ent/schema/mixin"
+)
+
+type AuditorMixin struct {
+	mixin.Schema
+}
+
+func (AuditorMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("createdByID").Optional().Nillable(),
+	}
+}
+func (AuditorMixin) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("createdByID"),
+	}
+}
+func (AuditorMixin) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("createdBy", User.Type).Field("createdByID").Unique(),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wenerme/ent
+
+go 1.16
+
+require entgo.io/ent v0.7.0 // indirect
