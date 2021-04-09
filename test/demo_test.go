fe3a4c794a2a2ade8927a5ed11feaeb407a32473
wener <wenermail@gmail.com>
2021-04-09 21:12:02 +0800

    add missing

---
+++ test/demo_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"context"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/wenerme/ent-demo/ent"
+	"github.com/wenerme/ent-demo/ent/enttest"
+	"log"
+	"testing"
+)
+
+func TestClear(t *testing.T) {
+	opts := []enttest.Option{
+		enttest.WithOptions(ent.Log(t.Log)),
+	}
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1", opts...)
+	client = client.Debug()
+	defer client.Close()
+	ctx := context.Background()
+
+	userA := client.User.Create().SetName("name").SaveX(ctx)
+	petA := client.Pet.Create().SetName("dog").SetOwningUserID(userA.ID).SaveX(ctx)
+	log.Println("clear user id")
+	petA.Update().ClearOwningUserID().SaveX(ctx)
+	log.Println("clear user")
+	petA.Update().ClearOwningUser().SaveX(ctx)
+}
